159f16e3d163e8d452d13a731bce50bf1135cdb7
Erik Searle <esearle@unbc.ca>
2018-09-15 11:59:21 -0700

    Flask setup
    Setup the directory structure, initialized the flask app, and wrote
    a basic readme. Database will still need to be initialized.
    
    -Erik Searle

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, render_template
+
+views = Blueprint('views', __name__, template_folder='templates')
+
+@views.route('/skema', methods=['GET'])
+def main_page():
+    return render_template('skema.html')
