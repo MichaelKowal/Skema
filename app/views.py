77015d4c65bcbf4afbf7c193b48dd9444f2152ef
Gr <morgan.graeme@gmail.com>
2018-09-20 13:51:51 -0700

    Side Navigation Working
    Side menu navigation has been implemented with some dummy buttons and search bars.

--- app/views.py
+++ app/views.py
@@ -5,3 +5,10 @@ views = Blueprint('views', __name__, template_folder='templates')
 @views.route('/skema', methods=['GET'])
 def main_page():
     return render_template('skema.html')
+
+
+'''
+@views.route('/info', methods=['GET'])
+def info():
+    return render_template('info.html')
+'''
