c8e80a274b2d6a0f6d5a79596802778f20a5f7a5
Gr <morgan.graeme@gmail.com>
2018-11-25 13:52:14 -0800

    New Buttons, Displays current semester

--- app/static/js/functions.js
+++ app/static/js/functions.js
@@ -128,19 +128,3 @@ function loadCalendar() {
 }
 
 
-$(document).ready(function() {
-    $('#bootstrapModalFullCalendar').fullCalendar({
-        events: [],
-        header: {
-            left: '',
-            center: 'prev title next',
-            right: ''
-        },
-        eventClick:  function(event, jsEvent, view) {
-            $('#modalTitle').html(event.title);
-            $('#modalBody').html(event.description);
-            $('#eventUrl').attr('href',event.url);
-            $('#fullCalModal').modal();
-        }
-    });
-});
