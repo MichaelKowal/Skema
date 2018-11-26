7cdbcf9de1937a0c6f745d47e555f51879fc15b2
Erik Searle <esearle@unbc.ca>
2018-11-25 17:41:37 -0800

    Courses only appear once in sidebar. Labs, tutorials separate.

--- app/static/js/functions.js
+++ app/static/js/functions.js
@@ -1,26 +1,30 @@
-var $calendar;
-var coursesInSideBar = [];
+let $calendar;
+let coursesInSideBar = [];
+let courseIdsInSideBar = {};
+let courseIdsInCalendar = [];
 
 function getCoursesFromServer(subject, professor, check100, check200, check300, check400, checkOther, displayedCoursesList) {
-    subjectString = subject.options[subject.selectedIndex].text;
-    profString = professor.options[professor.selectedIndex].text;
+    let subjectString = subject.options[subject.selectedIndex].text;
+    let profString = professor.options[professor.selectedIndex].text;
 
-    requestURL = "http://localhost:5000/data?subject=" + subjectString + "&prof=" + profString;
+    let requestURL = "http://localhost:5000/data?subject=" + subjectString + "&prof=" + profString;
     if(check100.checked === true) requestURL += "&level100=true";
     if(check200.checked === true) requestURL += "&level200=true";
     if(check300.checked === true) requestURL += "&level300=true";
     if(check400.checked === true) requestURL += "&level400=true";
     if(checkOther.checked === true) requestURL += "&levelOther=true";
 
-    var dataRequest = new XMLHttpRequest();
+    let dataRequest = new XMLHttpRequest();
     dataRequest.onreadystatechange = function() {
         if(this.readyState === 4 && this.status === 200) {
-            var coursesReturnedFromServer = JSON.parse(this.responseText);
+            let coursesReturnedFromServer = JSON.parse(this.responseText);
             coursesInSideBar = coursesReturnedFromServer.classes;
+            appendCourseTitleWithSection(coursesInSideBar);
+            collateCourseIds();
             displayedCoursesList.innerHTML = null;
-            var newDisplayedCoursesList = "";
-            for(var i=0; i<coursesInSideBar.length; i++){
-                newDisplayedCoursesList += "<li><a href=\"#\" onclick='addCourseToCalendar(this)'>" + coursesInSideBar[i]["title"] + "</a></li>\n";
+            let newDisplayedCoursesList = "";
+            for(let courseId in courseIdsInSideBar){
+                newDisplayedCoursesList += "<li><a href=\"#\" onclick='addCourseToCalendar(this)'>" + courseId + "</a></li>\n";
             }
             displayedCoursesList.innerHTML = newDisplayedCoursesList;
         }
@@ -29,13 +33,44 @@ function getCoursesFromServer(subject, professor, check100, check200, check300, 
     dataRequest.send();
 }
 
-function addCourseToCalendar(courseClicked){
-    var courseTitle = courseClicked.innerText;
-    for(var i=0; i<coursesInSideBar.length; i++){
-        if(courseTitle === coursesInSideBar[i]["title"]){
-            $calendar.fullCalendar('renderEvent', coursesInSideBar[i], true)
+function appendCourseTitleWithSection(dictionaries){
+    for(let i=0; i<dictionaries.length; i++){
+        let componentId = dictionaries[i]["component_id"];
+        if(componentId.charAt(0) === 'A'){
+            dictionaries[i]["title"] += " " + componentId;
+            dictionaries[i]["_id"] = dictionaries[i]["title"];
         }
+        else if(componentId.charAt(0) === 'T'){
+            dictionaries[i]["_id"] = dictionaries[i]["title"] + " Tutorials";
+            dictionaries[i]["title"] += " " + componentId;
+        }
+        else if(componentId.charAt(0) === 'L'){
+            dictionaries[i]["_id"] = dictionaries[i]["title"] + " Labs";
+            dictionaries[i]["title"] += " " + componentId;
+        }
+
     }
+}
+
+function collateCourseIds(){
+    for(let i=0; i<coursesInSideBar.length; i++){
+        courseIdsInSideBar[coursesInSideBar[i]["_id"]] = 1;
+    }
+}
+
+function addCourseToCalendar(courseClicked){
+    console.log(courseClicked.valueOf());
+    let courseId = courseClicked.innerText;
+    if(!courseIdsInCalendar.includes(courseId)) {
+        for (let i = 0; i < coursesInSideBar.length; i++) {
+            if (courseId === coursesInSideBar[i]["_id"]) {
+                $calendar.fullCalendar('renderEvent', coursesInSideBar[i], true);
+            }
+        }
+        courseIdsInCalendar.push(courseId);
+    }
+    courseClicked.value = "new value";
+    console.log(courseClicked.valueOf());
 }
 
 function loadCalendar() {
